56ae519930c40b3f20806c9b8a4365e0e2948e74
CGA <113306447+Cgaz275@users.noreply.github.com>
2025-12-17 08:24:35 +0700

    fix bug

--- DOAN/EVERSHOP/ShoesStore_Evershop/packages/evershop/src/modules/auth/pages/admin/adminLogin/index.ts
+++ DOAN/EVERSHOP/ShoesStore_Evershop/packages/evershop/src/modules/auth/pages/admin/adminLogin/index.ts
@@ -1,5 +1,4 @@
 import { buildUrl } from '../../../../../lib/router/buildUrl.js';
-import { setPageMetaInfo } from '../../../../cms/services/pageMetaInfo.js';
 
 export default (request, response, next) => {
   // Check if the user is logged in
@@ -8,10 +7,6 @@ export default (request, response, next) => {
     // Redirect to admin dashboard
     response.redirect(buildUrl('dashboard'));
   } else {
-    setPageMetaInfo(request, {
-      title: 'Admin Login',
-      description: 'Admin Login'
-    });
     next();
   }
 };
